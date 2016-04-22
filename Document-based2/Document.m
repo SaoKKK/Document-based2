886123362a9d54e0950cd98e93943749acd5726f
Saori Kohno <saorikohno@yahoo.com>
2016-04-23 00:04:52 +0900

    implemented unlocking document

--- Document-based2/Document.m
+++ Document-based2/Document.m
@@ -9,6 +9,8 @@
 #import "Document.h"
 #import "DocWinC.h"
 
+#define APPD (AppDelegate *)[NSApp delegate]
+
 @interface Document ()
 
 @end
@@ -39,12 +41,48 @@
     [self addWindowController:_docWinC];
 }
 
+//ドキュメントの保存
 - (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError{
     //PDFビューのドキュメントをNSDataにパッケージして返す
     DocWinC *winC = [[self windowControllers]objectAtIndex:0];
     return [winC pdfViewDocumentData];
 }
 
+- (void)saveDocument:(id)sender{
+    if ((APPD).isCopyLocked || (APPD).isPrintLocked) {
+        DocWinC *winC = [[self windowControllers]objectAtIndex:0];
+        (APPD).parentWin = winC.window;
+        (APPD).pwTxtPass.stringValue = @"";
+        (APPD).pwMsgTxt.stringValue = NSLocalizedString(@"UnlockEditMsg", @"");
+        (APPD).pwInfoTxt.stringValue = NSLocalizedString(@"UnlockEditInfo", @"");
+        [(APPD).parentWin beginSheet:(APPD).passWin completionHandler:^(NSInteger returnCode){
+            if (returnCode == NSModalResponseOK) {
+                [super saveDocument:nil];
+            }
+        }];
+    } else {
+        [super saveDocument:nil];
+    }
+}
+
+- (IBAction)saveDocumentAs:(id)sender{
+    if ((APPD).isCopyLocked || (APPD).isPrintLocked) {
+        DocWinC *winC = [[self windowControllers]objectAtIndex:0];
+        (APPD).parentWin = winC.window;
+        (APPD).pwTxtPass.stringValue = @"";
+        (APPD).pwMsgTxt.stringValue = NSLocalizedString(@"UnlockEditMsg", @"");
+        (APPD).pwInfoTxt.stringValue = NSLocalizedString(@"UnlockEditInfo", @"");
+        [(APPD).parentWin beginSheet:(APPD).passWin completionHandler:^(NSInteger returnCode){
+            if (returnCode == NSModalResponseOK) {
+                [super saveDocumentAs:nil];
+            }
+        }];
+    } else {
+        [super saveDocumentAs:nil];
+    }
+}
+
+//データを読み込んでドキュメントを開く
 - (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError {
     if ([self windowControllers].count != 0) {
         //復帰のための読み込みの場合
